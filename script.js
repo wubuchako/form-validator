db51d7c9aa3c2e80e5ab7eefc758c92d794c6484
github wubuchako <jfdev12@gmail.com>
2020-09-29 11:20:50 +0900

    validate form

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function processFormData(e){
+    // Using Contraint API
+    isValid = form.checkValidity();
+    // Style main message for an error
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    
+}
+
+// Event Listener
+form.addEventListener('submit', processFormData);
